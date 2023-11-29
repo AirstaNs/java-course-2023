bddd699663801ee7e65d497728b6419a370932e1
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2023-11-29 19:48:50 +0500

    fix task 4

--- src/main/java/edu/hw7/task4/PiMonteCarlo.java
+++ src/main/java/edu/hw7/task4/PiMonteCarlo.java
@@ -3,18 +3,20 @@ package edu.hw7.task4;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 public final class PiMonteCarlo {
 
-    private static final int TIMEOUT = 20;
     private static final double FOUR = 4.0;
 
     private PiMonteCarlo() {
     }
 
     public static double calculatePi(int totalIterations, int threadCount) {
+        if (threadCount <= 1) {
+            return calculatePiSingleThreaded(totalIterations);
+        }
+
         ExecutorService executor = Executors.newFixedThreadPool(threadCount);
         AtomicLong circleCount = new AtomicLong(0);
         AtomicLong totalCount = new AtomicLong(0);
@@ -23,27 +25,30 @@ public final class PiMonteCarlo {
             final int iterationsPerThread = totalIterations / threadCount;
 
             executor.submit(() -> {
-                long localCircleCount = 0;
-                for (int j = 0; j < iterationsPerThread; j++) {
-                    double x = ThreadLocalRandom.current().nextDouble();
-                    double y = ThreadLocalRandom.current().nextDouble();
-
-                    if (x * x + y * y <= 1) {
-                        localCircleCount++;
-                    }
-                }
-                circleCount.addAndGet(localCircleCount);
+                long points = calculatePointsInCircle(iterationsPerThread);
+                circleCount.addAndGet(points);
                 totalCount.addAndGet(iterationsPerThread);
             });
         }
-
-        executor.shutdown();
-        try {
-            executor.awaitTermination(TIMEOUT, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
+        executor.close();
         return FOUR * circleCount.get() / totalCount.get();
+    }
+
+    private static double calculatePiSingleThreaded(int totalIterations) {
+        long circleCount = calculatePointsInCircle(totalIterations);
+        return FOUR * circleCount / totalIterations;
+    }
+
+    private static long calculatePointsInCircle(int iterations) {
+        long circleCount = 0;
+        for (int i = 0; i < iterations; i++) {
+            double x = ThreadLocalRandom.current().nextDouble();
+            double y = ThreadLocalRandom.current().nextDouble();
+
+            if (x * x + y * y <= 1) {
+                circleCount++;
+            }
+        }
+        return circleCount;
     }
 }
--- src/test/java/edu/hw7/task4/PiMonteCarloTest.java
+++ src/test/java/edu/hw7/task4/PiMonteCarloTest.java
@@ -1,10 +1,18 @@
 package edu.hw7.task4;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PiMonteCarloTest {
-        private static final int CORES = Runtime.getRuntime().availableProcessors();
+
+    private static final int CORES = Runtime.getRuntime().availableProcessors();
+
     @Test
     void testPiApproximationAccuracy() {
         int totalIterations = 1000000;
@@ -23,5 +31,40 @@ class PiMonteCarloTest {
 
         assertTrue(Math.abs(piWithTwoThreads - Math.PI) < 0.01);
         assertTrue(Math.abs(piWithEightThreads - Math.PI) < 0.01);
+    }
+
+    private static Stream<Arguments> provideTestParameters() {
+        return Stream.of(
+                // Комбинации для 10 миллионов итераций
+                Arguments.of(10_000_000, 1),
+                Arguments.of(10_000_000, 6),
+                Arguments.of(10_000_000,  Runtime.getRuntime().availableProcessors()),
+                // Комбинации для 100 миллионов итераций
+                Arguments.of(100_000_000, 1),
+                Arguments.of(100_000_000, 6),
+                Arguments.of(100_000_000, Runtime.getRuntime().availableProcessors()),
+                // Комбинации для 1 миллиарда итераций
+                Arguments.of(1_000_000_000, 1),
+                Arguments.of(1_000_000_000, 6),
+                Arguments.of(1_000_000_000, Runtime.getRuntime().availableProcessors())
+            );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("provideTestParameters")
+    public void testPiCalculationPerformance(int totalIterations, int threadCount) {
+        Instant startTime = Instant.now();
+        double pi = PiMonteCarlo.calculatePi(totalIterations, threadCount);
+        Instant endTime = Instant.now();
+
+        Duration duration = Duration.between(startTime, endTime);
+        double seconds = duration.getSeconds() + duration.getNano() / 1e9;
+        double error = Math.abs(pi - Math.PI);
+
+        System.out.printf("Threads: %d, Iterations: %d, Time: %.2f s, Pi: %f, Error: %.9f%n",
+            threadCount, totalIterations, seconds, pi, error);
+
+        assertTrue(error < 0.01);
     }
 }
