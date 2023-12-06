c0f502d038bf87fefb08362f12eca9a38f897bad
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2023-12-06 07:31:22 +0500

    [task 2] fix close method

--- src/main/java/edu/hw8/task2/FixedThreadPool.java
+++ src/main/java/edu/hw8/task2/FixedThreadPool.java
@@ -2,17 +2,22 @@ package edu.hw8.task2;
 
 import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class FixedThreadPool implements ThreadPool, Executor {
     private final Thread[] threads;
     private final BlockingQueue<Runnable> taskQueue;
+    private final CountDownLatch latch;
     private volatile boolean isRunning = true;
 
     public FixedThreadPool(int nThreads) {
         threads = new Thread[nThreads];
         taskQueue = new LinkedBlockingQueue<>();
+        latch = new CountDownLatch(nThreads);
 
         for (int i = 0; i < nThreads; i++) {
             threads[i] = new Worker();
@@ -33,30 +38,40 @@ public class FixedThreadPool implements ThreadPool, Executor {
     public void execute(Runnable runnable) {
         if (isRunning) {
             taskQueue.add(runnable);
+        } else {
+            throw new RejectedExecutionException();
         }
     }
 
     @Override
     public void close() {
-        boolean isFinished = false;
-        while (!isFinished) {
-            isFinished = taskQueue.isEmpty();
-        }
-
+        final int timeout = 4;
         isRunning = false;
-        Arrays.stream(threads)
-            .forEach(Thread::interrupt);
+        if (taskQueue.isEmpty()) {
+            return;
+        }
+        try {
+            if (!latch.await(timeout, TimeUnit.SECONDS)) {
+                taskQueue.clear();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            Arrays.stream(threads).forEach(Thread::interrupt);
+        }
     }
 
-    private class Worker extends Thread {
+    private final class Worker extends Thread {
         public void run() {
-            while (isRunning) {
-                try {
+            try {
+                while (isRunning || !taskQueue.isEmpty()) {
                     Runnable task = taskQueue.take();
                     task.run();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
                 }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } finally {
+                latch.countDown();
             }
         }
     }
