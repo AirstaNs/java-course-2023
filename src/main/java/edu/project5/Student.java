60295eb0d35e95791ca96d8c66950e539c8b99cf
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2023-12-17 19:44:01 +0500

    done project-5

---
+++ src/main/java/edu/project5/Student.java
@@ -0,0 +1,13 @@
+package edu.project5;
+
+public final class Student {
+    private final String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
